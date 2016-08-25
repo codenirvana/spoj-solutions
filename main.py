48150fdbdaed78afa56c85d898f4dab7aea25f24
Udit <codenirvana28@gmail.com>
2016-08-25 23:46:47 +0530

    comments added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import  getpass
+import getpass
 import requests
 import grequests
 from bs4 import BeautifulSoup
@@ -8,13 +8,15 @@ session = requests.session()
 
 BASE_URL = "http://www.spoj.com"
 
+# find/create valid path
 def basePath():
     while 1:
         path = input('Enter path to save files: ').strip()
-
+        # complete home path
         if(path.startswith('~/')):
             path = path.replace( '~', os.path.expanduser('~'), 1  )
 
+        # create path if not exists
         if not os.path.exists(path):
             print('Path not exists: ' + path)
             permission = input('Do you want to create this path? (Y/N) ')
@@ -31,8 +33,10 @@ def basePath():
             continue
     return path + '/'
 
+
+# save source code files
 def createFiles(results, problemCode):
-    path = basePath()
+    path = basePath()   # path to save files
     print('Writing files...')
     total = len(results)
     for i in range( total ):
@@ -43,23 +47,28 @@ def createFiles(results, problemCode):
 
     print( 'Total files saved: ' + str(total) )
 
+
+# fetch all submissions
 def process(soup):
     problemCode = []
     problemUrl = []
 
     while 1:
-        rows = soup.select('.kol1 .statustext a.sourcelink')
+        rows = soup.select('.kol1 .statustext a.sourcelink')    # AC submissions
 
         for row in rows:
             code = row.get('data-pcode')
             if(code in problemCode):
-                continue
-            problemCode.append(code)
+                continue    # skip repeat submissions for same problem
+            problemCode.append(code)    # append problem code
+
+            # build source code url, /files/src/save/:ID
             url = row.get('data-url').split('/')
             url.insert(3, 'save')
             url = '/'.join( url )
-            problemUrl.append(url)
+            problemUrl.append(url)  # append problem url
 
+        # check for next page
         nextPage = soup.select('.pagination li')[-2].find('a')
         if(nextPage):
             print('Searching submissions...')
@@ -68,40 +77,43 @@ def process(soup):
         else:
             break
 
+    # fetch all problemUrl
     print('Fetching source code...')
     unsent_request = (grequests.get(BASE_URL + url, session=session) for url in problemUrl)
     results = grequests.map(unsent_request)
 
     createFiles(results, problemCode)
 
-def main():
-    USERNAME = input('User Name: ')
-    PASSWORD = getpass.getpass('Password: ')
-    URL = "/status/" + USERNAME + "/all/"
 
-    print('Logging in...')
+def main():
+    USERNAME = input('Username: ')
+    PASSWORD = getpass.getpass('Password: ')
 
     payload = {
         "login_user": USERNAME,
         "password": PASSWORD
     }
+
+    print('Logging in...')
     result = session.post(BASE_URL + "/login/", data=payload)
 
-    result = session.get(BASE_URL + URL)
+    # fetch user submissions page
+    result = session.get(BASE_URL + "/status/" + USERNAME + "/all/")
 
     soup = BeautifulSoup(result.text, "html.parser")
 
+    # successfully logged in, if logout button found
     logout_btn = soup.find("a", {"href": "/logout"})
-
     if not logout_btn:
         print('Failed!')
         return
 
+    # get user real name
     user = soup.find('a', href="/users/" + USERNAME).text.strip()[:-1]
-
     print('Hello ' + user)
 
     process(soup)
+
 
 if __name__ == '__main__':
     main()
